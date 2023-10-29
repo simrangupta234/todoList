2a3913f606fcec70c93fb5cd085d6cddf9049f34
unknown <simrangupta172002@gmail.com>
2023-10-29 22:28:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,73 +3,86 @@ import "./App.css";
 import TodoList from "./TodoList";
 
 const App = () => {
-  const [input, setinput] = useState("");
-  const [items, setitems] = useState([]);
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [filter, setFilter] = useState("all");
 
   const eventItem = (event) => {
-    setinput(event.target.value);
+    setInput(event.target.value);
   };
 
-  const additems = () => {
+  const addItems = () => {
     if (input.trim() !== "") {
-      setitems((olditems) => {
-        return [...olditems, input];
-      });
-      setinput("");
+      setItems((oldItems) => [...oldItems, input]);
+      setCompletedTasks((oldCompletedTasks) => [...oldCompletedTasks, false]); // Initially, the task is not completed.
+      setInput("");
     } else {
-      setinput("");
+      setInput("");
     }
   };
 
-
-
   const deleteItems = (id) => {
-    console.log("deleted");
+    setItems((oldItems) => oldItems.filter((arrElement, index) => index !== id));
+    setCompletedTasks((oldCompletedTasks) =>
+      oldCompletedTasks.filter((arrElement, index) => index !== id)
+    );
+  };
 
-    setitems((olditems) => {
-      return olditems.filter((arrElement, index) => {
-        return index !== id;
-      });
+  const toggleCompletion = (id) => {
+    setCompletedTasks((oldCompletedTasks) => {
+      return oldCompletedTasks.map((status, index) =>
+        index === id ? !status : status
+      );
     });
   };
 
-  const allTasks = () =>{
-    setitems((olditems) => {
-      return [...olditems, input];
-    });
-  }
   return (
     <div className="main">
       <div className="center">
         <h1>Todo List App</h1>
-        <select className="dropdown">
-          <option value="option1" onSelect={allTasks}>All Tasks</option>
-          <option value="option2" >Completed Tasks</option>
-          <option value="option3">Uncomplete Tasks</option>
+        <select
+          className="dropdown"
+          onChange={(event) => setFilter(event.target.value)}
+        >
+          <option value="all">All Tasks</option>
+          <option value="completed">Completed Tasks</option>
+          <option value="uncompleted">Uncompleted Tasks</option>
         </select>
         <br />
-        <div><input
-          className="input"
-          type="text"
-          placeholder="add a task"
-          value={input}
-          onChange={eventItem}
-        />
-        <button className="addbutton" onClick={additems}>
-          Add
-        </button></div>
-        
+        <div>
+          <input
+            className="input"
+            type="text"
+            placeholder="add a task"
+            value={input}
+            onChange={eventItem}
+          />
+          <button className="addbutton" onClick={addItems}>
+            Add
+          </button>
+        </div>
         <ol>
-          {items.map((itemVal, index) => {
-            return (
+          {items
+            .filter((_, index) => {
+              if (filter === "all") {
+                return true;
+              } else if (filter === "completed") {
+                return completedTasks[index] === true;
+              } else if (filter === "uncompleted") {
+                return completedTasks[index] === false;
+              }
+            })
+            .map((itemVal, index) => (
               <TodoList
                 key={index}
                 id={index}
                 text={itemVal}
                 onSelect={deleteItems}
+                onToggle={toggleCompletion}
+                isCompleted={completedTasks[index]}
               />
-            );
-          })}
+            ))}
         </ol>
       </div>
     </div>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,20 +2,29 @@ import React from "react";
 
 const TodoList = (props) => {
   return (
-    <>
-      <div className="inputItems">
-        <input className="checkbox" type="checkbox" />
-        <div className="inputItemsContainer">
-          <li className="inputItem">{props.text}</li>
-          <i
-            className="deleteIcon fa-solid fa-trash"
-            onClick={() => {
-              props.onSelect(props.id);
-            }}
-          ></i>
-        </div>
+    <div className="inputItems">
+      <input
+        className="checkbox"
+        type="checkbox"
+        checked={props.isCompleted}
+        onChange={() => props.onToggle(props.id)}
+      />
+      <div className="inputItemsContainer">
+        <li
+          className="inputItem"
+          style={{ textDecoration: props.isCompleted ? "line-through" : "none" }}
+        >
+          {props.text}
+        </li>
+        <i
+          className="deleteIcon fa-solid fa-trash"
+          onClick={() => {
+            props.onSelect(props.id);
+          }}
+        ></i>
       </div>
-    </>
+    </div>
   );
 };
+
 export default TodoList;
